646d0090b32810d5a0776cb5a716755f53adfd27
koki108 <kokitoya911@gmail.com>
2021-07-16 20:59:26 +0900

    掛け算と割り算足したわよ

--- team_hands-on/src/index.js
+++ team_hands-on/src/index.js
@@ -128,6 +128,20 @@
 	/* 演習の編集範囲 はじめ */
 
 	/* 演習1 ここから */
+	//掛け算用の関数 mul
+    function mul(){
+		eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
+    } 
+
+
+    //割り算用の関数 div
+    function div(){
+		if(parseFloat(midNum[1].value) == 0){
+			eqlNum.value = "ERROR";
+		}else{
+			eqlNum.value = parseFloat(midNum[0].value) / parseFloat(midNum[1].value);
+		}
+	}
 	// TODO: このコメントアウトを削除して書く
 	/* 演習1 ここまで */
 
